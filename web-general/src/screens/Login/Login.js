7b3e9a2d5e7165d1e6700f4fd29859d79f847666
Ventsislav Dimitrov <46657149+vents04@users.noreply.github.com>
2022-02-15 10:26:00 +0200

    login page inoti

--- web-general/src/screens/Login/Login.js
+++ web-general/src/screens/Login/Login.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react'
 
 import './Login.css'
 
+import { VscWorkspaceTrusted } from 'react-icons/vsc'
+
 export default class Login extends Component {
   render() {
     return (
-      <div>Login</div>
+      <div className="centered-content">
+        <div className="modal-box">
+          <div className="modal-topbar">
+            <div className="modal-topbar-left">
+              <div className="logo-container">
+                <VscWorkspaceTrusted  className="logo" size={20}/>
+                <p className="logo-title">Incendo</p>
+              </div>
+              <p className="page-title">Login</p>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
