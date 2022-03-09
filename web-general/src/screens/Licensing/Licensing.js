1f3d0f72121442fe4cf07311ffa44d82071fee83
vents04 <vencidim04@gmail.com>
2022-03-09 23:11:32 +0200

    licensing page added

--- web-general/src/screens/Licensing/Licensing.js
+++ web-general/src/screens/Licensing/Licensing.js
@@ -3,6 +3,8 @@ import Navbar from '../../components/Navbar/Navbar';
 
 import './Licensing.scss';
 
+import apache from '../../../assets/img/apache.png';
+
 export default class Licensing extends Component {
     render() {
         return (
@@ -10,6 +12,18 @@ export default class Licensing extends Component {
                 <Navbar activeMenu="licensing" />
                 <div className="page-contents-container">
                     <p className="page-title">Licensing</p>
+                    <p className="text">In order to ensure the trust in our platform, all our downloadable softwares are licensed under the open-source license Apache License 2.0</p>
+                    <div className="page-section">
+                        <div className="license">
+                            <div className="license-topbar">
+                                <img src={apache} className="license-image" />
+                                <p className="license-title">Apache License 2.0</p>
+                            </div>
+                            <a href="https://www.apache.org/licenses/LICENSE-2.0" target="_blank">
+                                <button className="action-button">Learn more</button>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
--- web-general/src/screens/PrivacyPolicy/PrivacyPolicy.js
+++ web-general/src/screens/PrivacyPolicy/PrivacyPolicy.js
@@ -10,6 +10,12 @@ export default class PrivacyPolicy extends Component {
                 <Navbar activeMenu="privacy-policy" />
                 <div className="page-contents-container">
                     <p className="page-title">Privacy policy</p>
+                    <p className="text">Incendo is a platform which relies on the publicity of all the data shared with us which is the main reason why nearly all of the data you have shared with us will be visible to other users.</p>
+                    <div className="page-section">
+                        <a href="https://resources.uploy.app/privacy-policy-incendo.html" target="_blank">
+                            <button className="action-button">Open the privacy policy</button>
+                        </a>
+                    </div>
                 </div>
             </div>
         )
