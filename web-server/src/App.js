d3b222475d55c43044f431fdea3a8f1aba62f7bc
vents04 <vencidim04@gmail.com>
2022-03-11 15:31:44 +0200

    calcs removed

--- web-server/src/App.js
+++ web-server/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import EditCampaign from './screens/EditCampaign/EditCampaign';
 import Home from './screens/Home/Home';
-import Index from './screens/Index/Index';
 import Login from './screens/Login/Login';
 import Signup from './screens/Signup/Signup';
 import ViewCampaign from './screens/ViewCampaign/ViewCampaign';
